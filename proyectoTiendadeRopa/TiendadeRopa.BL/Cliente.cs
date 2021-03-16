d783b3db616c4fe1aca04fc3e735158c20e2dc67
BV <BV@DESKTOP-BH11U8O>
2021-03-15 23:24:17 -0600

    Agregando controlador, Clase BL para las ordenes

---
+++ proyectoTiendadeRopa/TiendadeRopa.BL/Cliente.cs
@@ -0,0 +1,12 @@
+﻿namespace TiendadeRopa.BL
+{
+    public class Cliente
+    {
+        public object Activo { get; internal set; }
+        public object Direccion { get; internal set; }
+        public object Existente { get; internal set; }
+        public int Id { get; internal set; }
+        public object Nombre { get; internal set; }
+        public object Telefono { get; internal set; }
+    }
+}
